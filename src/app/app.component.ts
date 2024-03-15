e65b4c3a7fe6a13a75967003eaaa4da3bda13f84
alexDev2808 <colores.2808.8000@gmail.com>
2024-03-15 08:51:33 -0600

    Plantilla inicial del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Plantilla con JQuery
+declare var $:any;
+declare function HOME_INIT([]):any;
+declare function INIT_SWIPER([]):any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'streaming_tipo_netflix';
+
+  constructor() { }
+
+  ngOnInit(): void {
+      setTimeout(() => {
+        HOME_INIT($);
+        INIT_SWIPER($);
+      }, 50)
+  }
+  
 }
